425c74d6b914adf5e74f527830732b0997fc582f
Levi Ackerman <umeshshelare@gmail.com>
2024-02-25 02:34:38 +0530

    Added the functionality to Button Settings Menu & Dark - Light Mode

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,29 +5,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
+  // Variable to toggle settings menu height
+  settingsMenuHeight = false;
 
-  ngOnInit(): void {
+  // Variable to toggle dark mode
+  darkMode = false;
 
-
-
-
-    console.log("hello");
-
-
+  // Function to toggle settings menu height
+  settingsMenuToggle() {
+    // Toggle the settings menu height variable
+    this.settingsMenuHeight = !this.settingsMenuHeight;
   }
 
-  isDisplay = false;
-  toogledisp() {
-
-    this.isDisplay = !this.isDisplay;
-
-
+  // Function to toggle dark mode
+  toggleDarkMode() {
+    // Toggle the dark mode variable
+    this.darkMode = !this.darkMode;
+    // Add or remove 'dark-theme' class to body based on dark mode
+    if (this.darkMode) {
+      document.body.classList.add('dark-theme'); // Apply dark theme
+    } else {
+      document.body.classList.remove('dark-theme'); // Remove dark theme
+    }
   }
-
-  
-
-
-
 }
